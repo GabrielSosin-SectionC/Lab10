c318d874edb192221f07af4a1c17d4e673a23981
GabrielSosin-SectionC <156844536+GabrielSosin-SectionC@users.noreply.github.com>
2024-03-19 22:50:12 -0400

    Adding Main files

---
+++ lab10.js
@@ -0,0 +1,4 @@
+document.getElementById("submit").addEventListener("click", function() {
+    // console.log("1") //Used to check if function is working on click
+    document.getElementById("floatingTextarea2").value=""; //This is just a facade to make it seem like user is sending something to me :) 
+});
